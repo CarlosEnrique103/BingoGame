345bc47858f96720e0d493c32a1903dc053e7df1
juancarlos <juancarlosenrique103@gmail.com>
2021-05-19 11:04:42 -0500

    Added button generate numbers

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path')
-
 const app = express();
 
 app.use(function (req, res, next) {
@@ -9,21 +8,39 @@ app.use(function (req, res, next) {
 });
 
 
-// API
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.set(express.static('public'));
+
+// LOGIC
 
 let numbers = []
-const typeBingo = 'EE.UU';
-const lengthBingo = typeBingo== 'EE.UU'?  75 :  90;
+let length = 75;
 
-
-for (let i = 0; i < lengthBingo; i++) {
+for (let i = 1; i <= length; i++) {
     numbers.push(i);
 }
 
-app.get('/api/bingo/number', function (req, res) {    
-    res.send({"number": numbers[8]});
+
+// API
+
+const generateNumberRandom = (length) => {
+    return numberRandom = Math.floor(Math.random() * length) + 1;
+}
+
+app.get('/api/bingo/number', function (req, res) { 
+    let numberRandom = generateNumberRandom(length);
+    numbers = numbers.filter(n =>  n !== numberRandom );
+    --length;
+    res.send({number: numberRandom});
 });
 
+
+app.get('/api/bingo/primer', function (req, res) {
+
+    let numberRandom = Math.floor(Math.random() * lengthBingo) + 1;
+    res.send({number: numberRandom});
+});
 
 
 // GAME 
@@ -32,6 +49,11 @@ app.get('/game', function(req, res) {
   res.sendFile(path.join(__dirname, 'static/index.html'));
 });
 
+// Application
+
+app.use('/', (req, res) => {
+    return res.render('index');
+})
 
 app.listen(3000, function () {
     console.log("Listening on port 3000. Go to http://localhost:3000");
